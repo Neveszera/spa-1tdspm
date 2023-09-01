86ecad059da33c1f7b3136ce7faaa55cb0249129
Neveszera <gabrielgng26@gmail.com>
2023-09-01 09:05:59 -0300

    Iniciando a construção de rotas.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import Produtos from './routes/Produtos.jsx'
+import Erro404 from './routes/Erro404.jsx'
  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
