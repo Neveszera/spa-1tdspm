03b6abfcd4a2882567d60f955df2e7e888d40ec8
Neveszera <gabrielgng26@gmail.com>
2023-09-01 09:55:29 -0300

    Links dinâmicos, Hook useParams, reac-icons.

--- src/main.jsx
+++ src/main.jsx
@@ -11,16 +11,29 @@ import Erro404 from './routes/Erro404.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <App/>,
+    errorElement: <Erro404/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: '/produtos',
+        element: <Produtos/>,
+      },
+      {
+        path: '/editar/produtos/:id',
+        element: <EditarProdutos/>,
+      }
+    ]
   }
 ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <App />
-
+       <RouterProvider router={router}/>
   </React.StrictMode>,
 )
 
