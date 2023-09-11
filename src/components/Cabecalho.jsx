e50997c74aa3792c71f002a584ce7a7cad01299b
Neveszera <gabrielgng26@gmail.com>
2023-09-11 10:47:07 -0300

    deixando parelho com o conteudo da aula.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -8,20 +8,12 @@ export default function Cabecalho() {
         <h1>Vite + React / Coded By - RM552244</h1>
         <ul>
           <li>
-            <Link to="/"><H/> - HOME</Link>
+            <Link to="/">HOME - <H/></Link>
           </li>
           <li>
             <Link to="/produtos">PRODUTOS</Link>
           </li>
-          <li>
-            <Link to="/editar/produtos/1">PRODUTO - 1</Link>
-          </li>
-          <li>
-            <Link to="/editar/produtos/2">PRODUTO - 2</Link>
-          </li>
-          <li>
-            <Link to="/editar/produtos/3">PRODUTO - 3</Link>
-          </li>
+
         </ul>
       </header>
     </>
