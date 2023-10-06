883b1285574a2049ab444c6055aa89ed7c831400
Neveszera <gabrielgng26@gmail.com>
2023-10-06 08:53:24 -0300

    Listagem dinâmica com api externa.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,12 +3,29 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  return ( 
+  const [listaProdutoExterna, setListaProdutoExterna] = useState([{}]);
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/produtos",{
+      method: "GET",
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then((response)=> response.json())
+    .then((data) => {
+      setListaProdutoExterna(data);
+    })
+    .catch(error => console.log(error));
+  },[]);
+
+  return (
     <div>
       <h1>LISTA DE PRODUTOS</h1>
       <table className={style.tblEstilo}>
@@ -23,7 +40,7 @@ export default function Produtos() {
         </thead>
         <tbody>
         {
-          ListaProdutos.map((item,indice)=>(
+          listaProdutoExterna.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
                 <td>{item.id}</td>
                 <td>{item.nome}</td>
@@ -43,29 +60,23 @@ export default function Produtos() {
     </div>
   )
 }
-
 //Exemplo de useEfect
 // useEffect(()=>{
 //   console.log("Este useEffect renderiza sempre! " + counter);
 // });
-
 // const [counter, setCounter] = useState(0);
-
 // const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
-
 // useEffect(()=>{
 //   console.log("Este useEffect renderiza apenas uma vez!");
 //   setListaProdutosLocal(ListaProdutos);
 // },[ ]);
-
 // const [counter2, setCounter2] = useState(0)
-
 // useEffect(()=>{
 //   console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
 // },[counter2]);
-
 // <div>
 // <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
 // </div>
 // <div>
-// <button onClick={
+// <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+// </div>
