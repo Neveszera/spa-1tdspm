7db779e4030f149a0d13bb45b3f1d63f64e0440e
Neveszera <gabrielgng26@gmail.com>
2023-10-06 08:50:08 -0300

    API json-server instalada e configurada.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,53 +1,47 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import { GrFormEdit as Editar } from "react-icons/gr";
-import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
+import {GrFormEdit as Editar} from "react-icons/gr";
+import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
+
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  return (
+  return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
       <table className={style.tblEstilo}>
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>NOME</th>
-            <th>DESCRIÇÃO</th>
-            <th>PREÇO</th>
-            <th>EDITAR / EXCLUIR</th>
-          </tr>
+        <tr>
+          <th>ID</th>
+          <th>NOME</th>
+          <th>DESCRIÇÃO</th>
+          <th>PREÇO</th>
+          <th>EDITAR / EXCLUIR</th>       
+        </tr>
         </thead>
         <tbody>
-          {ListaProdutos.map((item, indice) => (
+        {
+          ListaProdutos.map((item,indice)=>(
             <tr key={indice} className={style.tblLine}>
-              <td>{item.id}</td>
-              <td>{item.nome}</td>
-              <td>{item.desc}</td>
-              <td>{item.valor}</td>
-              <td>
-                {" "}
-                <Link to={`/editar/produtos/${item.id}`}>
-                  <Editar />
-                </Link>{" "}
-                |{" "}
-                <Link to={`/excluir/produtos/${item.id}`}>
-                  <Excluir />
-                </Link>{" "}
-              </td>
+                <td>{item.id}</td>
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>{item.valor}</td>
+                <td> <Link to={`/editar/produtos/${item.id}`}><Editar/></Link> | <Link to={`/excluir/produtos/${item.id}`}><Excluir/></Link> </td>
             </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={4}>PRODUTOS LINDOS</td>
-          </tr>
-        </tfoot>
+          ))
+        }
+</tbody>
+<tfoot>
+  <tr>
+    <td colSpan={4}>PRODUTOS LINDOS</td>
+  </tr>
+</tfoot>
       </table>
     </div>
-  );
+  )
 }
 
 //Exemplo de useEfect
@@ -74,5 +68,4 @@ export default function Produtos() {
 // <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
 // </div>
 // <div>
-// <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-// </div>
+// <button onClick={
